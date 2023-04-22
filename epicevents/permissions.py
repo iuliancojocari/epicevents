b577728b9257e99efee99cc034d730f4a383ebd4
Iulian COJOCARI <iuliancojocari42@gmail.com>
2023-04-22 22:51:57 +0200

    bug fixex

--- epicevents/permissions.py
+++ epicevents/permissions.py
@@ -1,5 +1,7 @@
 from rest_framework import permissions
+from rest_framework.exceptions import PermissionDenied
 from users.models import MANAGEMENT, SALES, SUPPORT
+from .models import Client
 
 
 class IsManager(permissions.BasePermission):
@@ -14,12 +16,26 @@ class ClientPermissions(permissions.BasePermission):
             return request.method in permissions.SAFE_METHODS
         return request.user.team.name == SALES
 
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return (
+                request.user == obj.support_contact or request.user == obj.sales_contact
+            )
+        return request.user == obj.sales_contact
+
 
 class ContractPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.team.name == SUPPORT:
-            return False
+            return request.method in permissions.SAFE_METHODS
         return request.user.team.name == SALES
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return (
+                request.user == obj.support_contact or request.user == obj.sales_contact
+            )
+        return request.user == obj.sales_contact
 
 
 class EventPermissions(permissions.BasePermission):
@@ -27,3 +43,15 @@ class EventPermissions(permissions.BasePermission):
         if request.user.team.name == SUPPORT:
             return request.method in ("GET", "PUT")
         return request.user.team.name == SALES
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return (
+                request.user == obj.support_contact or request.user == obj.sales_contact
+            )
+        else:
+            if obj.event_status is True:
+                raise PermissionDenied("You cannot update a finished event.")
+            if request.user.team.name == SUPPORT:
+                return request.user == obj.support_contact
+            return request.user == obj.sales_contact
--- epicevents/views.py
+++ epicevents/views.py
@@ -18,21 +18,13 @@ from users.models import SALES, SUPPORT
 
 
 class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
     serializer_class = ClientSerializer
     permission_classes = [IsAuthenticated, IsManager | ClientPermissions]
     http_method_names = ["get", "post", "put", "delete"]
     filter_backends = [SearchFilter, DjangoFilterBackend]
     search_fields = ["^first_name", "^last_name", "^email"]
     filterset_fields = ["is_client", "date_created", "date_updated", "sales_contact"]
-
-    def get_queryset(self):
-        if self.request.user.team.name == SUPPORT:
-            return Client.objects.filter(
-                contract__event__support_contact=self.request.user
-            )
-        if self.request.user.team.name == SALES:
-            return Client.objects.filter(sales_contact=self.request.user)
-        return Client.objects.all()
 
     def destroy(self, request, *args, **kwargs):
         client = get_object_or_404(Client, id=self.kwargs["pk"])
@@ -43,6 +35,7 @@ class ClientViewSet(viewsets.ModelViewSet):
 
 
 class ContractViewSet(viewsets.ModelViewSet):
+    queryset = Contract.objects.all()
     serializer_class = ContractSerializer
     permission_classes = [IsAuthenticated, IsManager | ContractPermissions]
     http_method_names = ["get", "post", "put", "delete"]
@@ -56,12 +49,6 @@ class ContractViewSet(viewsets.ModelViewSet):
     ]
     filterset_fields = ["date_created", "status"]
 
-    def get_queryset(self):
-        if self.request.user.team.name == SALES:
-            return Contract.objects.filter(sales_contact=self.request.user)
-        elif self.request.user.team.name == SUPPORT:
-            return Contract.objects.filter(event__support_contact=self.request.user)
-
     def destroy(self, request, *args, **kwargs):
         contract = get_object_or_404(Contract, id=self.kwargs["pk"])
         contract.delete()
@@ -72,6 +59,7 @@ class ContractViewSet(viewsets.ModelViewSet):
 
 
 class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
     serializer_class = EventSerilizer
     permission_classes = [IsAuthenticated, IsManager | EventPermissions]
     http_method_names = ["get", "post", "put", "delete"]
@@ -83,15 +71,6 @@ class EventViewSet(viewsets.ModelViewSet):
         "^event_date",
     ]
     filterset_fields = ["date_created", "event_status"]
-
-    def get_queryset(self):
-        if self.request.user.team.name == SUPPORT:
-            return Event.objects.filter(support_contact=self.request.user)
-        elif self.request.user.team.name == SALES:
-            return Event.objects.filter(
-                client_contract__sales_contact=self.request.user
-            )
-        return Event.objects.all()
 
     def destroy(self, request, *args, **kwargs):
         event = get_object_or_404(Event, id=self.kwargs["pk"])
